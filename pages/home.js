84036a8ac44a8d38a650d928c2590224f25a85a6
sharanjeetkaur <sharanjeet.kaur@lt.hs-fulda.de>
2023-04-16 12:43:08 +0200

    next auth added

---
+++ pages/home.js
@@ -0,0 +1,10 @@
+import { useSession } from 'next-auth/react'
+
+export default function Home() {
+  const { data: session, status } = useSession()
+  return (
+    <div>
+      {session ? <p>You are logged in!</p> : <p>You are not logged in 😞</p>}
+    </div>
+  )
+}
